e844065a968936a000917763fd938056b5ca29a1
therogerwang <32213225+therogerwang@users.noreply.github.com>
2017-09-23 12:49:35 -0500

    Add files via upload

---
+++ actualHTMLcalender/pw.js
@@ -0,0 +1,9 @@
+
+function new_event() {
+
+    var start = document.getElementById("start").value;
+    var stop = document.getElementById("stop").value;
+
+    document.getElementById("Tue22").innerHTML = start;
+}
+

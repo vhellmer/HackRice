9ef4cef3646e4ec3e362c0310ae66dcf40b4a341
therogerwang <32213225+therogerwang@users.noreply.github.com>
2017-09-22 23:32:31 -0500

    Update Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -1,4 +1,22 @@
-def calendar():
-    return none
+#Calendar
 
+
+### stored list = [title, time, location, people, note, attachment]
+
+
+def add_event(list):
+    title = input("enter event name: ")
+    time = input("enter time: ")
+    location = input("enter location: ")
+    people  = input("enter people: ")
+    note = input("enter note: ")
+    attachment = input("enter attachment?")
+    return list.append([title, time, location, people, note ,attachment])
+
+def display(event):
+    return ("Your next scheduled event is... " + event[0] + " at " +event[1] + " at " + event[2] +" with " + event[3], "Note: " + event[4])
+
+calendar =[]
+add_event(calendar)
+print (display(calendar[0]))
 

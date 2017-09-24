98caf75940009ab8cd868bc8bf8e935ec8030a33
Joan2333 <32213228+Joan2333@users.noreply.github.com>
2017-09-23 22:21:41 -0500

    Add files via upload

--- pw.js
+++ pw.js
@@ -4,7 +4,7 @@ function new_event() {
     var day = document.getElementById("day").value;
     var time = document.getElementById("time").value
     var dayTime = day.concat(time)
-
+    
     document.getElementById(dayTime).innerHTML = event;
 }
 function cancel_event(){
